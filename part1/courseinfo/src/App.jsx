abd0e894a312c70fe532de47aa01ea7a94aa12a9
Humberto Bernal <bemh.1997@gmail.com>
2025-09-28 18:47:21 -0600

    Refactor exercises 1.1 - 1.5 courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,70 +1,20 @@
-import PropTypes from 'prop-types';
+const Header = (props) => <h1>{props.course}</h1>;
 
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
-};
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+);
 
-Header.propTypes = {
-  course: PropTypes.string.isRequired,
-};
-const Content = (props) => {
-  return (
-    <>
-      <h2>Content:</h2>
-      <ul>
-      <li>
-          <Part number='1' part={props.part1} exercises={props.exercises1}/> 
-      </li>
-      <li>
-          <Part number='2' part={props.part2} exercises={props.exercises2}/> 
-      </li>
-      <li>
-          <Part number='3' part={props.part3} exercises={props.exercises3}/> 
-      </li>   
-      </ul> 
-    </>
-  )
-};
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
 
-Content.propTypes = {
-  part1: PropTypes.string.isRequired,
-  part2: PropTypes.string.isRequired,
-  part3: PropTypes.string.isRequired,
-  exercises1: PropTypes.number.isRequired,
-  exercises2: PropTypes.number.isRequired,
-  exercises3: PropTypes.number.isRequired,
-};
-
-const Part = (props) =>{
-  return (
-      <>
-        <p>Part {props.number}: {props.part}, exercises: {props.exercises}.</p>
-      </>
-    )
-}
-
-Part.propTypes = {
-  number: PropTypes.string.isRequired,
-  part: PropTypes.string.isRequired,
-  exercises: PropTypes.number.isRequired,
-};
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}.</p>
-    </>
-  )
-};
-Total.propTypes = {
-  exercises1: PropTypes.number.isRequired,
-  exercises2: PropTypes.number.isRequired,
-  exercises3: PropTypes.number.isRequired,
-};
+const Total = (props) => <p>Number of exercises {props.total}</p>;
 
 const App = () => {
   const course = {
@@ -72,35 +22,32 @@ const App = () => {
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
       },
       {
-        name:'Using props to pass data',
-        exercises: 7
+        name: 'Using props to pass data',
+        exercises: 7,
       },
       {
         name: 'State of a component',
-        exercises: 14
-      }
-    ]
+        exercises: 14,
+      },
+    ],
   }
 
   return (
-    <>
+    <div>
       <Header course={course.name} />
-      <Content 
-        part1={course.parts[0].name} 
-        exercises1={course.parts[0].exercises} 
-        part2={course.parts[1].name} 
-        exercises2={course.parts[1].exercises} 
-        part3={course.parts[2].name} 
-        exercises3={course.parts[2].exercises} />
-      <Total 
-        exercises1={course.parts[0].exercises} 
-        exercises2={course.parts[1].exercises} 
-        exercises3={course.parts[2].exercises} />
-    </>
+      <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
+    </div>
   )
 };
 
-export default App
+export default App;
